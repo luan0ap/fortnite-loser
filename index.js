7a9c68d130b93705023f66605d04b1c62ca7487b
Pedro Maia <pedrommone@gmail.com>
2018-10-09 19:45:58 -0300

    Dispatch calls

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const config = require('dotenv').config(),
     request = require('request'),
-    cache = require("redis").createClient(process.env.REDIS_URL);
+    cache = require("redis").createClient(process.env.REDIS_URL),
+    totalVoice = require('totalvoice-node');
+
+const totalVoiceClient = new totalVoice(process.env.TOTAL_VOICE_KEY);
 
 const CACHE_KEY = 'fortnite-loses-count';
 
@@ -17,8 +20,33 @@ let getNumberOfLosses = function (response) {
     return totalMatches - wins;
 };
 
+let randomizeAudio = function () {
+    let audios = [
+        'https://www.myinstants.com/media/sounds/cetemdemenciaringtone.mp3',
+        'https://www.myinstants.com/media/sounds/naofazmal.mp3',
+        'https://www.myinstants.com/media/sounds/vai-ganhar-vai-perder-perdeu-ganhoooou.mp3',
+        'https://www.myinstants.com/media/sounds/moises-nao-consegue-programa-do-silvio-santos.mp3'
+    ];
+
+    return audios[Math.floor(Math.random() * audios.length)];
+};
+
+let call = function (audio) {
+    console.log(`Sent audio: ${audio}.`);
+
+    totalVoiceClient.audio.enviar(process.env.TARGET_TELEPHONE, audio)
+        .then(function (data) {
+            console.log('Call has been scheduled.');
+        })
+        .catch(function (error) {
+            console.log('Failed to schedule call.');
+        });
+};
+
 let notify = function () {
     console.log('Target has been notified!');
+
+    call(randomizeAudio());
 };
 
 let notifyTarget = function (totalLooses) {
@@ -43,7 +71,7 @@ let processRequestResponse = function (err, res, body) {
     notifyTarget(totalLosses);
 };
 
-const options = {
+const requesUserData = {
     url: `https://api.fortnitetracker.com/v1/profile/${process.env.TARGET_PLATFORM}/${process.env.TARGET_USERNAME}`,
     method: 'GET',
     headers: {
@@ -52,6 +80,6 @@ const options = {
     }
 };
 
-let execute = () => request(options, processRequestResponse);
+let execute = () => request(requesUserData, processRequestResponse);
 
 setInterval(execute, 5000);
